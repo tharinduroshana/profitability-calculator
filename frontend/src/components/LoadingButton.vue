9742c5d2a6d579dc9405e31b5677cb624a018b94
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-09-25 23:13:14 +0300

    - Added Loading button component

---
+++ frontend/src/components/LoadingButton.vue
@@ -0,0 +1,32 @@
+<script>
+import Button from "@/components/Button.vue";
+
+/*
+* Loading button
+* @text: Text for the loading button component
+*
+* This component visualizes a loading button to show the users something's working in the background
+* */
+export default {
+  name: "LoadingButton",
+  props: {
+    text: {
+      type: String,
+    }
+  },
+  components: {Button}
+}
+</script>
+
+<template>
+  <div class="form-btn">
+    <button class="submit-btn">
+      <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
