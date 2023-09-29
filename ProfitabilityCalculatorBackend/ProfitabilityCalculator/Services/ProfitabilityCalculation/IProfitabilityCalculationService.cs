ec00a0a395f476c93e7b9713500b491629503d93
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-09-29 11:12:07 +0300

    - Broken down profitability calculation steps to relevant methods

--- ProfitabilityCalculatorBackend/ProfitabilityCalculator/Services/ProfitabilityCalculation/IProfitabilityCalculationService.cs
+++ ProfitabilityCalculatorBackend/ProfitabilityCalculator/Services/ProfitabilityCalculation/IProfitabilityCalculationService.cs
@@ -5,4 +5,7 @@ namespace ProfitabilityCalculator.Services.ProfitabilityCalculation;
 public interface IProfitabilityCalculationService
 {
     ProfitabilityCalculationResponse CalculateProfitability(Models.ProfitabilityCalculation profitabilityCalculation);
+    double CalculateTotalDistanceBasedCosts(double pricePerKilometre, double noOfKilometres);
+    double CalculateTotalTimeBasedCosts(double pricePerHour, double noOfHours);
+    double CalculateTotalCosts(double totalDistanceBasedCosts, double totalTimeBasedCosts);
 }
--- ProfitabilityCalculatorBackend/ProfitabilityCalculator/Services/ProfitabilityCalculation/ProfitabilityCalculationService.cs
+++ ProfitabilityCalculatorBackend/ProfitabilityCalculator/Services/ProfitabilityCalculation/ProfitabilityCalculationService.cs
@@ -7,15 +7,30 @@ public class ProfitabilityCalculationService : IProfitabilityCalculationService
     public ProfitabilityCalculationResponse CalculateProfitability(
         Models.ProfitabilityCalculation profitabilityCalculation)
     {
-        var costPerKilometres = profitabilityCalculation.PricePerKilometre * profitabilityCalculation.NoOfKilometres;
-        var costPerHours = profitabilityCalculation.PricePerHour * profitabilityCalculation.NoOfHours;
-        var totalCost = costPerKilometres + costPerHours;
+        var totalDistanceBasedCosts = CalculateTotalDistanceBasedCosts(profitabilityCalculation.PricePerKilometre, profitabilityCalculation.NoOfKilometres);
+        var totalTimeBasedCosts = CalculateTotalTimeBasedCosts(profitabilityCalculation.PricePerHour, profitabilityCalculation.NoOfHours);
+        var totalCost = CalculateTotalCosts(totalDistanceBasedCosts, totalTimeBasedCosts);
 
         var profitability = profitabilityCalculation.Income - totalCost;
 
         return new ProfitabilityCalculationResponse(profitabilityCalculation.Id,
             profitabilityCalculation.PricePerKilometre, profitabilityCalculation.PricePerHour,
             profitabilityCalculation.NoOfKilometres, profitabilityCalculation.NoOfHours,
-            profitabilityCalculation.Income, costPerKilometres, costPerHours, profitability);
+            profitabilityCalculation.Income, totalDistanceBasedCosts, totalTimeBasedCosts, profitability);
+    }
+
+    public double CalculateTotalDistanceBasedCosts(double pricePerKilometre, double noOfKilometres)
+    {
+        return pricePerKilometre * noOfKilometres;
+    }
+
+    public double CalculateTotalTimeBasedCosts(double pricePerHour, double noOfHours)
+    {
+        return pricePerHour * noOfHours;
+    }
+
+    public double CalculateTotalCosts(double totalDistanceBasedCosts, double totalTimeBasedCosts)
+    {
+        return totalDistanceBasedCosts + totalTimeBasedCosts;
     }
 }
